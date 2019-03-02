70b6b85fb75fef8b77dccd41247a44151dadd77e
Bouachrine <47561940+Bouachrine@users.noreply.github.com>
2019-03-02 15:42:44 +0100

    Update ConnectionBDD.java

--- connectionpoool/src/connectionpoool/ConnectionBDD.java
+++ connectionpoool/src/connectionpoool/ConnectionBDD.java
@@ -7,20 +7,16 @@ public class ConnectionBDD {
             public static ResultSet res;
             protected static Connection connect = null;
            
-          
-
                 
             public static Connection getInst(){
             	try {
-                String url="jdbc:mysql://localhost:3306/maao";
+                String url="jdbc:mysql://localhost:3306/test";
                 String user="root";
                 String password="";
 
                 Object newInstance = Class.forName("com.mysql.jdbc.Driver");
             	connect= DriverManager.getConnection(url, user, password);
-            	/*
-            	JOptionPane.showMessageDialog(null,"The Connection is successfully established");
-            */
+
             	}
                 catch (Exception e) {
                     e.printStackTrace();
@@ -29,59 +25,7 @@ public class ConnectionBDD {
                 }
                  return connect;  
             }
-            
-    
-            /*
-            public static void main(String [] args ) {
-            	ConnectionBDD c = new ConnectionBDD();
-            	c.getInst();
-            	c.closeInstance(c.getInst());
-            }*/
-            /*
-            
-            //Méthode requete de recherche
-            public static ResultSet rechercher(String query){
-                try {
-                res=state1.executeQuery(query);
-                }
-                catch (Exception e) {
-                e.printStackTrace();
-                }
-                return res;
-                } 
-            //Méthode d'ajout
-            public static void ajouter(String query){
-                try {
-                state1.executeUpdate(query);
-                System.out.println("Element ajouté a la base");
-                }
-                catch (Exception e) {
-                e.printStackTrace();
-                }     
-                
-                }   
-            
-            //Méthode de modification
-            public static void modifier(String query){
-                try {
-                state1.executeUpdate(query);
-                System.out.println("Element Modifié");
-                }
-                catch (Exception e){
-                e.printStackTrace();
-                }   
-                }   
-            //Méthode de supprimer
-            public static void supprime(String query){
-                try {
-                state1.executeUpdate(query);
-                System.out.println("Element supprimé");     
-                }
-                catch (Exception e) {
-                e.printStackTrace();
-                }   
-                }
-                */
+ 
 
 
 }
